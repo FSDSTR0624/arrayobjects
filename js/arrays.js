0f4b16c29cf22cd80cceb64124e7c5db2e2dc40c
Jordi Galobart Salvat <jgalobart@gmail.com>
2024-07-01 09:11:55 +0200

    first commit

---
+++ js/arrays.js
@@ -0,0 +1,35 @@
+// add item to the end of the array (push)
+
+// extracts an item from the end of the array (pop)
+
+// extracts an item from the beginning (shift)
+
+// add items to the beginning of the array (unshift)
+
+// remove an element in a specific position (splice)
+
+// replace an element in a specific position (splice)
+
+// get a portion of array (slice)
+
+// concatenate arrays (concat)
+
+// search in an array (indexOf / lastIndexOf / includes)
+
+// display every element of an array (forEach)
+
+// display every element of an array (map)
+
+// sort an array of texts (sort)
+
+// sort an array of numbers (sort)
+
+// sort an array of texts with special characters (sort + localeCompare)
+
+// reverse the order of elements in one array (reverse)
+
+// split a string into an array (split)
+
+// join an array to a string (join)
+
+// sum all items of the array (reduce)
---
+++ js/objects.js
@@ -0,0 +1,15 @@
+// Display all movies
+
+// Show the number of movies we have in the array
+
+// Search information for specific movie (find)
+
+// How many movies were shooted after 1980
+
+// Display only the title of each movie (map version)
+
+// Display only the title of each movie (forEach version)
+
+// Display only the title of each movie sorted by year (without modifying the original array)
+
+// Display the average year for the array of movies
